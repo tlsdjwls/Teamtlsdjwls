d08586ae60fa65b336c200e5454a286e7ed59fea
tlsdjwls <tlsdjwls366@gmail.com>
2021-04-23 13:32:19 +0900

    local2

--- BlackJackProject/src/com/tlsdjwls/biz/Dealer.java
+++ BlackJackProject/src/com/tlsdjwls/biz/Dealer.java
@@ -10,40 +10,65 @@ import com.tlsdjwls.biz.model.DeckVO;
 import com.tlsdjwls.biz.model.ScoreVO;
 import com.tlsdjwls.biz.service.DeckService;
 
-public class Dealer extends DeckService {
+public class Dealer{
   protected List<DeckVO> deckList;
-  protected List<ScoreVO> scoreList;
+  protected List<ScoreVO> scList;
   protected Random rnd;
   private static final int card = 16; // 딜러 카드가 16이하일경우 무조건뽑아야함
-  private static final String name = "딜러";
+  private static final String name = "딜러"; // 이름 설정 딜러
   
   
-  public Dealer() {
-	deckList = new ArrayList<DeckVO>();
-	scoreList = new LinkedList<ScoreVO>();
-	
-	
-  }
-  public void hit(String name) {
-	  // TODO 딜러 카드 드로우
-	  System.out.println("=".repeat(50));
-	  System.out.println("카드 뽑기");
-	  System.out.println("=".repeat(50));
-	 
-	  
-  }
+	public Dealer() {
+		deckList = new ArrayList<DeckVO>(); // 댁 리스트 VO 로 받아오기
+		}
+
+	public void hit(String name) {
+		// TODO 딜러 카드 드로우
+		for (DeckVO vo : deckList) {
+		System.out.println("=".repeat(50));
+		System.out.println(name + "카드 뽑기");
+	    System.out.println("=".repeat(50));
+		Integer Num = rnd.nextInt(21) + 1; // 랜덤카드 선택후 카드를 뽑았을때 16이하이면 카드를 무조건 뽑아야함
+		for (int i = 0; i < card; i++) {
+			if (Num > card) {
+				System.out.println(card + "보다 점수가 적습니다 한장뽑아주세요");
+			 } else if (Num < card + i) {
+				System.out.println("카드를 뽑겠습니까?");
+			  }
+		}
+		vo.getCardShap(); // 딜러 카드를 vo 값에 저장
+		vo.getCardNum();
+		deckList.add(vo);
+		this.hit(name);
+	}
+
+	}
+
+	public void stay(String name) {
+		System.out.println(name + "더이상 카드를 뽑을수 없습니다");
+
+	}
+    public void DealerPoint() {
+    // TODO 딜러점수 를 vo에 저장
+    	System.out.println("=".repeat(50));
+    	System.out.println("딜러 점수");
+    	System.out.println("=".repeat(50));
+        for(DeckVO voi : deckList) {
+    	System.out.println(voi.getDealerPoint());
+    	deckList.add(voi);
+    	}
+        for(ScoreVO vni : scList) {
+        System.out.println(vni.getWin());
+        System.out.println(vni.getLose());
+        scList.add(vni);
+        }
+    	this.DealerPoint();
+    }
+    
+} 	
+    
   
-  public void prentDealer() {
-	  Integer Num = rnd.nextInt(20) + 1;
-	  for(int i = 0 ; i < card ; i++) {
-	  if(Num > card) {
-	   System.out.println(card + "보다 카드가 적습니다 한장뽑아주세요");	  
-	  }else if(Num < card + i) {
-	   System.out.println("카드를 뽑겠습니까?");
-	  }
-	   
-	  }
-  }
   
   
-}
+  
+
--- BlackJackProject/src/com/tlsdjwls/biz/service/DeckService.java
+++ BlackJackProject/src/com/tlsdjwls/biz/service/DeckService.java
@@ -14,14 +14,14 @@ public class DeckService {
 	   DeckVO deckVO;
 	   Scanner scan;
 
+	  
 	   public DeckService() {
 	      cardlist = new ArrayList<String>();
 	      cardlist2 = new ArrayList<DeckVO>();
 	      rnd = new Random();
 	      scan = new Scanner(System.in);
-	      
-	   }
 
+	   }
 	   public void cardDeck() {
 
 	      String pattern[] = { "heart", "spade", "clover", "dia" };
@@ -34,7 +34,6 @@ public class DeckService {
 	      this.cardCheck();
 	   }
 
-	   
 	   public void cardCheck() {
 	      int num = rnd.nextInt(52);
 	      String card;
@@ -57,117 +56,12 @@ public class DeckService {
 	      cardVO.setCardShap(card);
 	      cardVO.setScorePoint(cardscore);
 	      cardlist2.add(cardVO);
-	   }
-	   
-	   public Integer sumScore() {
-	         Integer sumScore = 0;
-	         for (DeckVO vo ) {
-	            sumScore += vo.getUserPoint();
-	         }
-	         return sumScore;
-	      }
-
-	      public void hit() {
-	         // user hit
-	         System.out.println("유저가 덱에 카드 한장을 추가했습니다");
-	         // 덱 추가
-//	         mdList.add(pdList.get(1));
-//	         pdList.remove(1);
-
-	         // :D 확인용
-//	         System.out.println("=".repeat(50));
-//	         System.out.println("유저의 덱 현황");
-//	         System.out.println("-".repeat(50));
-	//
-//	         for (DeckVO vo : mdList) {
-//	            System.out.println(vo);
-//	         }
-//	         System.out.println("-".repeat(50));
-//	         System.out.println("총점 : " + this.sumScore()); //
-	      }
-
-	      public void stay() {
-
-	         System.out.println("유저가 더이상 덱에 카드를 추가하지 않습니다");
-	      }
-
-	      public void user() {
-	         
-	         while (true) {
-	            System.out.println("카드를 한장 더 추가하겠습니까? 0 = Hit / 1 = Stay");
-	            System.out.print(">> ");
-	            String input = scan.nextLine();
-	            Integer nInput = null;
-	            try {
-	               nInput = Integer.valueOf(input);
-	            } catch (NumberFormatException e) {
-	               // TODO Auto-generated catch block
-	               e.printStackTrace();
-	            }
-	            if (nInput == 0) {
-	               this.hit();
-	            } else if (nInput == 1) {
-	               this.stay();
-	            }
-	            break;
-	         }
-
-	      }
 	      
-	      public void pointBust() {
-	         // TODO 버스트와 승패 확인
-	         // VO에 담긴 카드값을 dealer 와 user 변수에 담고
-	         // boolean을 이용해 if() 조건식으로 bust인지 참과 거짓으로 판별
-	         int dealer = deckVO.getDealerPoint();
-	         boolean dealerBust = false;
-	         if(dealer > 21) {
-	            dealerBust = true;
-	         }
-	         int user = deckVO.getUserPoint();
-	         boolean userBust = false;
-	         if(user > 21) {
-	            userBust = true;
-	         }
-	         
-	         // 먼저 딜러와 플레이어의 카드값을 공개
-	         System.out.println("딜러 : " + dealer );
-	         System.out.println("플레이어 : " + user);
-	         
-	         // if() 사용해서 각 카드값을 비교하고
-	         // bust 상태를 확인하고 승패 판별하는 메시지 출력
-	         if(dealer == user || dealerBust && userBust) {
-	            System.out.println("무승부");
-	         }
-	         if(dealer < user || dealerBust ) {
-	            System.out.println("플레이어 승");
-	         } else {
-	            System.out.println("딜러 승");
-	         }
-	         this.printPoint();
-	      }
+	   }
 
-	      public void printPoint() {
-	         // TODO 승패 후 카드 출력
-	         
-	         /*
-	          * dealerList와 userList에 담긴 값들을 변수 Size에 담고
-	          * for() 반복문으로 리스트에 담긴 값들을 출력
-	          */
-	         System.out.println("Dealer Deck");
-	         int dSize = dealerList.size();
-	         for( int i = 0 ; i < dSize ; i++) {
-	            System.out.print(dealerList.get(i) +"\t");
-	         }
-	         
-	         System.out.println("=".repeat(50));
-	         
-	         System.out.println("Player Deck");
-	         int uSize = userList.size();
-	         int userPoint;
-	         for(int i = 0; i < uSize ; i++) {
-	            System.out.print(userList.get(i) + "\t");
-	         }
-	         
-	      }
+	   public void sumScore() {
+
+	   }
+
 
 }
